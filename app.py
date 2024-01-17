e4231512557af12d05b987dc4bed7fec26233c48
Nun <nun.lukkarinen01@gmail.com>
2024-01-17 18:01:17 +0200

    committed

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+# app.py
 from flask import Flask
-from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
+db = SQLAlchemy(app)
+
 
 import routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
