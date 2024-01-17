7b17ab1867891342267c2aa416a2f98d26faad59
Nun <nun.lukkarinen01@gmail.com>
2024-01-17 13:33:59 +0200

    Modified

--- app.py
+++ app.py
@@ -9,3 +9,24 @@ app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://postgres:1305@localhost:5432/postgres'
 
+
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
