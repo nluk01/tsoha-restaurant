56781d9b511ff2b5ebfd1f27146722274ce86ee7
Nun <nun.lukkarinen01@gmail.com>
2024-01-17 14:16:51 +0200

    modified

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import redirect, render_template, request, session
 from os import getenv
+from dotenv import load_dotenv
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
+
+load_dotenv()
+
 app.secret_key = getenv("SECRET_KEY")
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 
 import routes
