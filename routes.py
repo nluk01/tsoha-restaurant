e6dc3203a9cebe21e4a21b5243fda404ab2dcb57
Nun <nun.lukkarinen01@gmail.com>
2024-01-17 14:04:52 +0200

    Modified

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from app import app
+from users import users
 from flask import render_template, redirect, render_template, request, session
 
 
@@ -7,14 +8,32 @@ from flask import render_template, redirect, render_template, request, session
 def index():
     return render_template("index.html")
 
-@app.route("/login",methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
-    return redirect("/") 
 
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
